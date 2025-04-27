5b7c1bd1b317717505d68b773a227bf6c4d45401
Shams M. Monem <shamsmmonem@gmail.com>
2025-04-27 19:06:30 +0300

    Frontend API url added in Dockerfile, Added Serial instead if int in autoincrement pk for Postgres, added schema.sql to init db

--- database/schema.sql
+++ database/schema.sql
@@ -1,22 +1,24 @@
+-- schema.sql
+-- This SQL script creates the database schema for e-market system.
+-- This is in PostgreSQL syntax, for SQLite, replace `SERIAL PRIMARY KEY` with `INTEGER PRIMARY KEY`
+
 CREATE TABLE ACCOUNT
 (
   password_hash VARCHAR(255) NOT NULL,  -- Storing hashed passwords as strings
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,         -- Email addresses are text
   balance DECIMAL(10,2) NOT NULL,      -- Monetary value should be decimal
-  account_id INT,
-  PRIMARY KEY (account_id)
+  account_id SERIAL PRIMARY KEY
 );
 
 CREATE TABLE PRODUCT
 (
-  product_id INT NOT NULL,
+  product_id SERIAL PRIMARY KEY,
   name VARCHAR(100) NOT NULL,          -- Product names are text
   description TEXT NOT NULL,           -- Descriptions can be longer text
   price DECIMAL(10,2) NOT NULL,        -- Prices should be decimal for accuracy
   on_sale BOOLEAN NOT NULL,            -- Sale status is true/false
   creator_id INT NOT NULL,
-  PRIMARY KEY (product_id),
   FOREIGN KEY (creator_id) REFERENCES ACCOUNT(account_id)
 );
 
