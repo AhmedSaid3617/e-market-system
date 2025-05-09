3f657decfd68d1c666d79208999b711fd7c9f54b
Shams M. Monem <shamsmmonem@gmail.com>
2025-05-09 11:27:43 +0300

    Add SQL queries for Citus monitoring and analysis

---
+++ database/reports.sql
@@ -0,0 +1,6 @@
+EXPLAIN ANALYZE SELECT COUNT(*) FROM ACCOUNT;
+SELECT * FROM pg_dist_shard;
+SELECT * FROM pg_dist_node;
+SELECT * FROM citus_get_active_worker_nodes();
+SELECT * FROM citus_tables;
+SELECT * FROM pg_dist_shard_placement;
